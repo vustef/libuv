2fd81b325cd9f1bcf3836373806035a8781e143b
Jameson Nash <vtjnash@gmail.com>
2019-05-31 19:59:06 +0000

    win,loop: support thread migration
    (cherry picked from commit bc03abac5cd1fe4493ac03db48c395acbb3f6d0a)
    (cherry picked from commit 045a186f370190bb477b3bbfc95427661da8d514)

--- src/win/core.c
+++ src/win/core.c
@@ -120,7 +120,7 @@ int uv_loop_init(uv_loop_t* loop) {
   uv__once_init();
 
   /* Create an I/O completion port */
-  loop->iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 1);
+  loop->iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
   if (loop->iocp == NULL)
     return uv_translate_sys_error(GetLastError());
 
